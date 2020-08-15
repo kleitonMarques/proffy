3d814422b88465e0066432fd13e7c5c9bb19da78
Kleiton Marques <kleitonm.marques@gmail.com>
2020-08-15 15:59:32 -0300

    Styling form mobile for page teacher list

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 import './styles.css'
 
 function TeacherList() {
+    const imageAvatar = "https://avatars1.githubusercontent.com/u/32112630?s=460&u=933db4ca92aaf181148b90668a98c394da8d2fc6&v=4"
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
@@ -25,6 +29,34 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src={imageAvatar} alt="Kleiton Marques"/>
+                        <div>
+                            <strong>Kleiton Marques</strong>
+                            <span>Química</span>
+                        </div>
+                    </header>
+                    <p>
+                        Estusiasta das melhores tecnologias de química avançada.
+                        <br/><br/>
+                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                    </p>
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="Whatsapp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
+            
         </div>
     )
 }
