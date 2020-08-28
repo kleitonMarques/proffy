0de448aa0f7ee94ff524497c91318ddae7d23860
Kleiton Marques <kleitonm.marques@gmail.com>
2020-08-28 18:06:06 -0300

    Structuring HTML of Registration page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
+import Input from '../../components/input'
+
+import WarningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css'
 
@@ -15,22 +18,27 @@ function TeacherForm() {
             <main>
                 <fieldset>
                     <legend>Seus dados</legend>
-
-                    <div className="input-block">
-                        <label htmlFor="name">Nome completo</label>
-                        <input type="text" id="name" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="avatar">Avatar</label>
-                        <input type="text" id="avatar" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="whatsapp">Whatsapp</label>
-                        <input type="text" id="whatsapp" />
-                    </div>
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
                 </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Input name="subject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <footer>
+                     <p>
+                        <img src={WarningIcon} alt="Aviso importante"/>
+                        Importante! <br/>
+                        Preencha todos os dados
+                     </p>
+                     <button type="button">
+                        Salvar cadastro
+                     </button>
+                </footer>
             </main>
         </div>
     )
