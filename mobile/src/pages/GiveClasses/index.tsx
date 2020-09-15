f539c9ad7403dc727b26935795775c627982125b
Kleiton Marques <kleitonm.marques@gmail.com>
2020-09-15 14:31:59 -0300

    Create navigation

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Image, Text, View, TouchableOpacity } from 'react-native'
+
+
+
+function GiveClasses() {
+    return (
+        <View />
+    )
+}
+
+export default GiveClasses
