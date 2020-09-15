48043b20cdb98905e2748c9baf64d9ee384026d8
Kleiton Marques <kleitonm.marques@gmail.com>
2020-09-15 15:54:35 -0300

    Developing internal pages

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing() {
         navigate('GiveClasses')
     }
 
+    function handleNavigateToStudyPage() {
+        navigate('Study')
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -26,7 +30,7 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton onPress={handleNavigateToStudyPage} style={[styles.button, styles.buttonPrimary]}>
                     <Image source={stydyIcon} />
                     <Text style={styles.buttonText}>Estudar</Text>
                 </RectButton>
