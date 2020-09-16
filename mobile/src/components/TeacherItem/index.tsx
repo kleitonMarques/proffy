204573a990f1a5e5ac628143bb30f981ff63dfbc
Kleiton Marques <kleitonm.marques@gmail.com>
2020-09-16 18:10:05 -0300

    Styling component Teacher Item

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Image, Text, View } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+
+import heartOutline from '../../assets/images/icons/heart-outline.png'
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
+import whatsappIcon from '../../assets/images/icons/whatsapp.png'
+
+import styles from './styles'
+
+function TeacherItem() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.profile}>
+                <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/32112630?s=460&u=933db4ca92aaf181148b90668a98c394da8d2fc6&v=4' }} />
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>Kleiton Marques</Text>
+                    <Text style={styles.subject}>Matemática</Text>
+                </View>
+            </View>
+
+            <Text style={styles.bio}>
+                Matematico;
+                {'\n'}
+                Fisico
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/Hora {'   '} 
+                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={styles.favoriteButton}>
+                        <Image source={heartOutline} />
+                    </RectButton>
+
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}>Entrar em contato</Text>
+                    </RectButton>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default TeacherItem
