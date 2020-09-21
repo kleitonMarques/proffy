3473df359b89fa457015d98c141e5bd75e4f3bcf
Kleiton Marques <kleitonm.marques@gmail.com>
2020-09-21 09:29:22 -0300

    End application

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
+import React, { useState } from 'react'
 import { View, ScrollView } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
 
 import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
 
 import styles from './styles'
 
 function Favorites() {
+    const [favorites, setFavorites] = useState([])
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if(response) {
+                const favoritedTeacher = JSON.parse(response)
+
+                setFavorites(favoritedTeacher)
+            }
+        })
+    }
+
+    useFocusEffect(() => {
+        loadFavorites()
+    })
+
     return (
         <View style={styles.container}>
             <PageHeader title="Meus proffys favoritos" />
@@ -15,12 +33,12 @@ function Favorites() {
                 paddingHorizontal: 16,
                 paddingBottom: 24,
             }}>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-            </ScrollView>            
+                {favorites.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem key={teacher.id} teacher={teacher} favorited />
+                    )
+                })}
+            </ScrollView>
         </View>
     )
 }
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import { View, ScrollView, Text, TextInput } from 'react-native'
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
 import { Feather } from '@expo/vector-icons'
 import AsyncStorage from '@react-native-community/async-storage'
+import { useFocusEffect } from '@react-navigation/native'
 
 
 import PageHeader from '../../components/PageHeader'
@@ -33,6 +34,10 @@ function TeacherList() {
             }
         })
     }
+
+    useFocusEffect(() => {
+        loadFavorites()
+    })
 
     function handleToggleFiltersVisible() {
         setIsFilterVisible(!isFilterVisible)
